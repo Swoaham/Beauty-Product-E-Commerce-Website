1b7cdd9c447270ade20c48fa1949d828cbb3a362
swoaham <upadhyayswoaham3@gmail.com>
2025-09-25 09:59:34 +0530

    Add custom 404.html fallback for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit } from '@angular/core';
 import { ProductService } from './product.service';
 import { Product } from './product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'beautyproduct';
-
   
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const redirectPath = sessionStorage.getItem('redirect');
+    if (redirectPath) {
+      sessionStorage.removeItem('redirect');
+      // navigate using Angular router
+      this.router.navigateByUrl(redirectPath);
+    }
+  }
   
 }
